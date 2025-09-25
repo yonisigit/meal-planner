2994a64efd056ab0b8059d9cbc314416ff361564
yonisigit <yonisirote@gmail.com>
2025-09-25 21:33:25 +0300

    setup

---
+++ backend/src/server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app =  express();
+
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
